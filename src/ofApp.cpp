9f41b73d95f0acf06ab8e42b72e2101a1052ab29
taptouch17 <31634464+taptouch17@users.noreply.github.com>
2017-11-08 14:57:27 -0400

    Initial Commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,96 @@
+#include "ofApp.h"
+#include <random>
+
+using namespace std;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+    
+    random_device rDev;
+    mt19937 rgen(rDev());
+    uniform_int_distribution<float> distribution(0,1200);
+    uniform_int_distribution<float> distribution1(0.970);
+    
+    for(auto i = 0u; i < 20; i++){
+    distribution(X);
+        if (i == 10) {
+    distribution1(Y);
+        }
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+ofBackground(0);
+
+for(auto i = 0u; i < X.size(); i++)
+ {
+   jewel.draw(X[i], Y[i]);
+ }
+}
+
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
